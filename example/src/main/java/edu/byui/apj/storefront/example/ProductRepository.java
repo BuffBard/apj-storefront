0e6cc88b1064647172b16823f455739a622fa73a
BuffBard <tjalapenos@gmail.com>
2026-03-09 16:20:34 -0600

    Tutorial 10-1

---
+++ example/src/main/java/edu/byui/apj/storefront/example/ProductRepository.java
@@ -0,0 +1,10 @@
+package edu.byui.apj.storefront.example;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+
+
+}
